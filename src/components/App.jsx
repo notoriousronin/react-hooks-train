2269419eed611860ac7dd6929ba44e72cbcc91ad
Andrey Gavrilyuk <andrew250519@gmail.com>
2023-01-26 16:46:18 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,43 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const App = () => {
   return (
     <div>
       <HookSwitcher />
+      <Notification />
     </div>
   );
+};
+
+const Notification = () => {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setVisible(false);
+    }, 2000);
+    return () => clearTimeout(timeOut);
+  }, []);
+
+  return <div>{visible && <p>Hello</p>}</div>;
 };
 
 const HookSwitcher = () => {
   const [color, setColor] = useState('violet');
 
   const [fontSize, setFontSize] = useState(14);
+
+  useEffect(() => console.log('Mount'), []);
+  // вызывается только 1 раз при создании компонента, аналогично componentDidMount
+
+  useEffect(() => console.log('Update'));
+  // аналогично componentDidUpdate, будет вызываться каждый раз, когда компонент будет обновлятся
+
+  useEffect(() => {
+    console.log('Mount');
+    return () => console.log('Unmount');
+  }, []);
+  // аналогично componentWillUnmount
 
   return (
     <div
